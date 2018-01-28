17b72a4047de10455b95e47a7baee4e09ea4a23d
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-27 21:04:14 -0500

    Made menu and allowed for interaction with the pet

--- src/main/java/virtualPet/VirtualPetApp.java
+++ src/main/java/virtualPet/VirtualPetApp.java
@@ -13,18 +13,32 @@ public class VirtualPetApp {
 			int thirst = myPet.getThirst();
 			int waste = myPet.getWaste();
 			int boredom = myPet.getBoredom();
-			
-			System.out.println("1. ");
-			System.out.println("2. ");
-			System.out.println("3. ");
-			System.out.println("4. ");
-			System.out.println("5. Do nothing");
-			
+			String name = myPet.getName();
+
+			System.out.println(name);
 			System.out.println("Hunger: " + hunger);
 			System.out.println("Thirst: " + thirst);
 			System.out.println("Waste: " + waste);
 			System.out.println("Boredom: " + boredom);
 			System.out.println();
+			System.out.println("Menu");
+			System.out.println("1. Feed " + name);
+			System.out.println("2. Water " + name);
+			System.out.println("3. Bathroom " + name);
+			System.out.println("4. Play " + name);
+			System.out.println("5. Do nothing");
+			int option = input.nextInt();
+			// feed, water, play, bathroom
+			if (option == 1) {
+				myPet.feed();
+			} else if (option == 2) {
+				myPet.water();
+			} else if (option == 3) {
+				myPet.bathroom();
+			} else if (option == 4) {
+				myPet.play();
+			}
+
 			myPet.tick();
 		}
 
