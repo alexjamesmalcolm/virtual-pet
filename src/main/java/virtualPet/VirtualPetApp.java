b9a04bdd4b4442db75fcb364bdda618a0a9bab40
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-27 19:15:23 -0500

    Created method displayPetStats

--- src/main/java/virtualPet/VirtualPetApp.java
+++ src/main/java/virtualPet/VirtualPetApp.java
@@ -1,10 +1,33 @@
 package virtualPet;
 
+import java.util.Scanner;
+
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		VirtualPet myPet = new VirtualPet();
 
+		boolean isAlive = true;
+
+		displayPetStats(myPet);
+
+		// while (!isAlive) {
+		// }
+
+		input.close();
+	}
+
+	static void displayPetStats(VirtualPet pet) {
+		int hunger = pet.getHunger();
+		int thirst = pet.getThirst();
+		int waste = pet.getWaste();
+		int boredom = pet.getBoredom();
+
+		System.out.println("Hunger: " + hunger);
+		System.out.println("Thirst: " + thirst);
+		System.out.println("Waste: " + waste);
+		System.out.println("Boredom: " + boredom);
 	}
 
 }
