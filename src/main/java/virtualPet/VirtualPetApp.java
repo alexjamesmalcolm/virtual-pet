1423811eafc52363f2f8bbc34c5081110126c599
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-28 04:51:14 -0500

    Did some refactoring

--- src/main/java/virtualPet/VirtualPetApp.java
+++ src/main/java/virtualPet/VirtualPetApp.java
@@ -29,39 +29,7 @@ public class VirtualPetApp {
 			}
 
 			while (myPet.alive() && !myPet.hasEscaped()) {
-				int hunger = myPet.getHunger();
-				int thirst = myPet.getThirst();
-				int waste = myPet.getWaste();
-				int boredom = myPet.getBoredom();
-				String name = myPet.getName();
-
-				System.out.println(name);
-				System.out.println("Hunger:  " + progressBar(hunger));
-				System.out.println("Thirst:  " + progressBar(thirst));
-				System.out.println("Waste:   " + progressBar(waste));
-				System.out.println("Boredom: " + progressBar(boredom));
-				System.out.println();
-				System.out.println("1. Feed " + name);
-				System.out.println("2. Refill " + name + "'s water");
-				System.out.println("3. Let " + name + " go to the bathroom");
-				System.out.println("4. Play with " + name);
-				System.out.println("5. Do nothing");
-				System.out.println("6. Exit");
-				int option = input.nextInt();
-				// feed, water, play, bathroom
-				myPet.tick();
-				if (option == 1) {
-					myPet.feed();
-				} else if (option == 2) {
-					myPet.water();
-				} else if (option == 3) {
-					myPet.bathroom();
-				} else if (option == 4) {
-					myPet.play();
-				} else if (option == 6) {
-					input.close();
-					System.exit(0);
-				}
+				gameLoop(input, myPet);
 			}
 
 			if (!myPet.alive()) {
@@ -89,6 +57,54 @@ public class VirtualPetApp {
 		}
 
 		input.close();
+	}
+
+	private static void gameLoop(Scanner input, VirtualPet myPet) {
+		displayStats(myPet);
+
+		System.out.println();
+
+		int option = requestAction(input, myPet);
+
+		myPet.tick();
+		if (option == 1) {
+			myPet.feed();
+		} else if (option == 2) {
+			myPet.water();
+		} else if (option == 3) {
+			myPet.bathroom();
+		} else if (option == 4) {
+			myPet.play();
+		} else if (option == 6) {
+			input.close();
+			System.exit(0);
+		}
+	}
+
+	private static int requestAction(Scanner input, VirtualPet myPet) {
+		String name = myPet.getName();
+		System.out.println("1. Feed " + name);
+		System.out.println("2. Refill " + name + "'s water");
+		System.out.println("3. Let " + name + " go to the bathroom");
+		System.out.println("4. Play with " + name);
+		System.out.println("5. Do nothing");
+		System.out.println("6. Exit");
+		int option = input.nextInt();
+		return option;
+	}
+
+	private static void displayStats(VirtualPet myPet) {
+		int hunger = myPet.getHunger();
+		int thirst = myPet.getThirst();
+		int waste = myPet.getWaste();
+		int boredom = myPet.getBoredom();
+		String name = myPet.getName();
+
+		System.out.println(name);
+		System.out.println("Hunger:  " + progressBar(hunger));
+		System.out.println("Thirst:  " + progressBar(thirst));
+		System.out.println("Waste:   " + progressBar(waste));
+		System.out.println("Boredom: " + progressBar(boredom));
 	}
 
 	public static String progressBar(int percentage) {
